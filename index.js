55497f379fce97163bd427e5a0dfe20918b16019
Connie-oyoyo <123514207+Connie-oyoyo@users.noreply.github.com>
2023-03-22 08:13:29 +0100

    Updated temperature units

--- index.js
+++ index.js
@@ -49,12 +49,16 @@ function press(event) {
 function displayFahrenheitTemperature(event)  {
     event.preventDefault();
     let temperatureElement = document.querySelector("#temperature");
+    celsiusLink.classList.remove("active");
+    fahrenheitLink.classList.add("active");
     let fahrenheitTemperature = (celsiusTemperature * 9) / 5 + 32;
     temperatureElement.innerHTML = Math.round(fahrenheitTemperature);
 }
 
 function displayCelsiusTemperature(event) {
     event.preventDefault();
+    celsiusLink.classList.add("active");
+    fahrenheitLink.classList.remove("active");
     let celsiusTemperature = document.querySelector("#temperature");
     temperatureElement.innerHTML = (celsiusTemperature);
 }
@@ -71,5 +75,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 
-
-search("Lagos");
